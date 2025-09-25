55f31263a504dc5595e0cc08bebc91aea3c668de
Jared de Jesus Olazaran Lopez <2430022@upv.edu.mx>
2025-09-24 20:34:48 -0600

    Agregue las funciones faltantes y simplifique el codigo

--- main.cpp
+++ main.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <string>
-#include <thread>
 
 using namespace std;
 
 int codigo;
+int limite=10;
 int indice;
 int pmc=0;
+bool validar=true;
 string npmc;
-int id[10]={101,102,103,104,105,106,107,108,109,110};
-string nombre[10]={"Martillo de bola    ",
-                   "Destornillador      ",
-                   "Cinta metrica       ",
-                   "Llave inglesa       ",
-                   "Taladro inalambrico ",
-                   "Sierra circular     ",
+int id[100]={101,102,103,104,105,106,107,108,109,110};
+string nombre[100]={"Martillo de bola",
+                   "Destornillador",
+                   "Cinta metrica",
+                   "Llave inglesa",
+                   "Taladro inalambrico",
+                   "Sierra circular",
                    "Caja de herramientas",
-                   "Tijeras             ",
-                   "Tornillos           ",
-                   "Clavos              "
+                   "Tijeras",
+                   "Tornillos",
+                   "Clavos"
                 };
-int stock[10]={50,100,45,65,90,34,78,98,120,150};
-float precio[10]={15.00,8.75,20.00,25.99,100.00,1350.00,200.00,50.00,5.00,5.00};
+int stock[100]={50,100,45,65,90,34,78,98,120,150};
+float precio[100]={15.00,8.75,20.00,25.99,100.00,1350.00,200.00,50.00,5.00,5.00};
 
 void consultarProducto();
-//void actualizarInventario();
-//void generarReporte();
-//void productoMasCaro();
-
+void actualizarInventario();
+void generarReporte();
+void productoMasCaro();
+void agregarProductos();
 int main() {
 
     
@@ -36,29 +37,33 @@ int main() {
 
     while(true){
         cout<<"Que accion desea realizar?"<<endl;
-
-        cout<<"1.- Consultar productos"<<endl;
-        cout<<"2.- Actualizar inventario"<<endl;
-        cout<<"3.- Generar reporte completo"<<endl;
-        cout<<"4.- Encontrar el producto mas caro"<<endl;
-        cout<<"5.- Salir"<<endl;
+        
+        cout<<"1.- Agregar articulo"<<endl;
+        cout<<"2.- Consultar productos"<<endl;
+        cout<<"3.- Actualizar inventario"<<endl;
+        cout<<"4.- Generar reporte completo"<<endl;
+        cout<<"5.- Encontrar el producto mas caro"<<endl;
+        cout<<"6.- Salir"<<endl;
         int opcion;
         cin>>opcion;
 
         switch(opcion){
             case 1:
-            consultarProducto();
+            agregarProductos();
             break;
             case 2:
-            //actualizarInventario();
+            consultarProducto();
             break;
             case 3:
-            //generarReporte();
+            actualizarInventario();
             break;
             case 4:
-            //productoMasCaro();
+            generarReporte();
             break;
             case 5:
+            productoMasCaro();
+            break;
+            case 6:
             cout<<"Gracias por usar el sistema de la ferreteria 'El martillo'"<<endl;
             return 0;
             break;
@@ -70,33 +75,118 @@ int main() {
     return 0;
 }
 
-void consultarProducto(){
-    cout << "Ingresa el codigo del producto a consultar: " << endl;
-    cin >> codigo;
+/*void conseguirIndice(){
+    for(int i=0;i<100;i++){
+        if(nombre[i].equals(null)){
+            limite = i;
+            break;
+        }
+    }
+}*/
+
+void validacion(){
     if (cin.fail()) {
         cin.clear();
         cin.ignore(10000, '\n');
         cout << "Por favor ingrese un codigo valido." << endl;
-        cout<<"Redirigiendo al menu principal..."<<endl;
-        this_thread::sleep_for(chrono::seconds(2));
-        system("clear");
         return;
     }
-    for(int i=0;i<10;i++){
+}
+
+void validacionWhile(){
+    if (cin.fail()) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "Por favor ingrese un numero valido." << endl;
+        validar = true;
+    }else{
+        validar = false;
+    }
+}
+
+void consultarProducto(){
+    cout << "Ingresa el codigo del producto a consultar: " << endl;
+    cin >> codigo;
+    validacion();
+    for(int i=0;i<limite;i++){
         if(codigo==id[i]){
             indice=i;
         }
     }
     if(codigo!=id[indice]){
         cout<<"El codigo ingresado no existe, intente de nuevo"<<endl;
-        cout<<"Redirigiendo al menu principal..."<<endl;
-        this_thread::sleep_for(chrono::seconds(2));
         return;
     }
     cout<<"Codigo: "<<id[indice]<<endl;
     cout<<"Nombre: "<<nombre[indice]<<endl;
     cout<<"Stock: "<<stock[indice]<<endl;
     cout<<"Precio: $"<<precio[indice]<<endl;
-    cout<<"Redirigiendo al menu principal..."<<endl;
-    this_thread::sleep_for(chrono::seconds(5));
+}
+
+void actualizarInventario(){
+    cout<<"Ingresa el codigo del articulo a actualizar: ";
+    cin>>codigo;
+
+    validacion();
+    for(int i=0;i<limite;i++){
+        if(codigo==id[i]){
+            indice=i;
+        }
+    }
+    if(codigo!=id[indice]){
+        cout<<"El codigo ingresado no existe, intente de nuevo"<<endl;
+        return;
+    }
+
+    cout<<"Producto encontrado: " << nombre[indice]<<endl;
+    do{
+      cout<<"Ingrese el nuevo numero de stock, stock actual: "<< stock[indice]<<endl;
+      cin>>stock[indice];
+      validacionWhile();
+    }while(validar==true);
+    do{
+      cout<<"Ingrese el nuevo precio, precio actual: $"<<precio[indice]<<endl;
+      cin>>precio[indice];
+      validacionWhile();
+    }while(validar==true);
+}
+
+void generarReporte(){
+     cout << "---------------Reporte de inventario---------------" << endl;
+cout << "Codigo\t|\tNombre\t\t|\tStock\t|\tPrecio" << endl;
+cout << "--------------------------------------------------------" << endl;
+for (int i = 0; i <limite; i++) {
+    cout << id[i] << "\t|\t" 
+         << nombre[i] << "\t\t|\t" 
+         << stock[i] << "\t|\t$"
+         << precio[i] << endl;
+}
+
+}
+
+void productoMasCaro(){
+    for(int i=0;i<limite;i++){
+        if(precio[i]>pmc){
+            pmc=precio[i];
+            npmc=nombre[i];
+        }
+    }
+    cout<<"El producto mas caro es: "<<npmc<<", con un precio de: $"<<pmc<<endl;
+}
+
+void agregarProductos(){
+    cout<<"Ingresa el nombre de tu producto: "<<endl;;
+    cin>>nombre[limite];
+    do{
+      cout<<"Ingresa la cantidad de stock del producto"<<endl;
+      cin>>stock[limite];
+      validacionWhile();
+    }while(validar==true);
+    do{
+      cout<<"Ingresa el precio para el producto: "<<endl;
+      cin>>precio[limite];
+      validacionWhile();
+    }while(validar==true);
+    id[limite]=id[limite-1]+1;
+    limite++;
 }
